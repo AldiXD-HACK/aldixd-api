6bae03f7e7c50eb152d9a926693e924d7927608a
AldiXD-HACK <aldixd2009@gmail.com>
2025-08-10 13:40:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  telegramToken: process.env.TELEGRAM_TOKEN || '', // Can use env var
+  telegramToken: process.env.TELEGRAM_TOKEN || '',
   UrlCreate: process.env.URL_CREATE || 'your-panel-domain.com',
   FolderPanel: process.env.FOLDER_PANEL || 'panel-folder',
   UrlWebp: process.env.URL_WEBP || 'your-webp-domain.com',
-  FolderWebp: process.env.FOLDER_WEBP || 'webp-folder'
+  FolderWebp: process.env.FOLDER_WEBP || 'webp-folder',
+  creator: process.env.CREATOR || 'AldiXDCodeX🇲🇨'
 };
