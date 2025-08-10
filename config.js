4762d3d215890be49aaa51f1c49fbe2575fc681c
AldiXD-HACK <aldixd2009@gmail.com>
2025-08-10 13:21:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  telegramToken: '', // Leave empty to set via web interface
+  UrlCreate: 'your-panel-domain.com',
+  FolderPanel: 'panel-folder',
+  UrlWebp: 'your-webp-domain.com',
+  FolderWebp: 'webp-folder'
+};
